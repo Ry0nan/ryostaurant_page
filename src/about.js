89b87080ae908da0f46a567e7b30a1f2cd4f3523
Ry0nan <125460295+Ry0nan@users.noreply.github.com>
2025-09-23 17:57:08 +0800

    added a temp about page

---
+++ src/about.js
@@ -0,0 +1,15 @@
+//import if needed
+
+function loadAboutPage(){
+    // get the html content area
+    const contentArea = document.querySelector('#content');
+
+    // temp heading
+    const aboutHeading = document.createElement('h1');
+    aboutHeading.textContent = "What is Umastaurant even about?";
+    aboutHeadingHeading.className = "about-heading";
+    contentArea.append(aboutHeading);
+}
+
+// export Menu section
+export default loadAboutPage;

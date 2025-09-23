70a6c87bb306e0ef618755859d19d058a7414d84
Ry0nan <125460295+Ry0nan@users.noreply.github.com>
2025-09-23 16:40:46 +0800

    added an Image in the dom

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 console.log("Hello! I am running!")
 
+// import css
 import   "./styles.css";
+// import assets
+import SeiunSkyWallpaper from './assets/images/umastaurant_wallpaper.webp';
 
 // get the html content area
 const contentArea = document.querySelector('#content');
@@ -12,6 +15,12 @@ mainHeading.className = "hero-title";
 contentArea.append(mainHeading);
 
 //paragraph?
-const subHeading = doocument.createElement('p');
-subHeading = "Welcome to our Umamusume themed restaurant!";
+const subHeading = document.createElement('p');
+subHeading.textContent = "Welcome to our Umamusume themed restaurant!";
 contentArea.append(subHeading)
+
+//image?
+const mainWallpaper = document.createElement('img');
+mainWallpaper = SeiunSkyWallpaper;
+mainImage.alt = "Our Umastaurant Wallpaper";
+contentArea.append(mainWallpaper);

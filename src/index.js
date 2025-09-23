60e2acb3269bdedb476952154bb5213fa921f0a7
Ry0nan <125460295+Ry0nan@users.noreply.github.com>
2025-09-23 16:25:09 +0800

    added a heading and a subheading to the DOM

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 console.log("Hello! I am running!")
 
 import   "./styles.css";
+
+// get the html content area
+const contentArea = document.querySelector('#content');
+
+// heading?
+const mainHeading = document.createElement('h1');
+mainHeading.textContent = "Welcome to Umastaurant";
+mainHeading.className = "hero-title";
+contentArea.append(mainHeading);
+
+//paragraph?
+const subHeading = doocument.createElement('p');
+subHeading = "Welcome to our Umamusume themed restaurant!";
+contentArea.append(subHeading)

de7c1c784f3da5a16071e05817227e0275cbc977
Ry0nan <125460295+Ry0nan@users.noreply.github.com>
2025-10-03 20:51:55 +0800

    changed deployment and build configurations for easier development

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
+const isProduction = process.env.NODE_ENV == 'production';
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
+        publicPath: isProduction ? '/ryostaurant_page/' : '/',
+    },
+    devServer: {
+    static: './dist',
     },
     plugins: [
         new HtmlWebpackPlugin({

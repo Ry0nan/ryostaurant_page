6c56e7177b6f0d88ee9044301359bbcab7f11c96
Ry0nan <125460295+Ry0nan@users.noreply.github.com>
2025-10-04 22:51:14 +0800

    configured to add audio files

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
                 use: ["style-loader", "css-loader"],
             },
             {
-                test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+                test: /\.(png|svg|jpg|jpeg|gif|webp|mp3|ogg|wav)$/i,
                 type: 'asset/resource',
             },
         ]
